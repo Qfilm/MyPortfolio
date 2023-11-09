1ff310f6d271044c2253c86995e02a6c60a15fff
Dileep <dileepvunnam@gmail.com>
2023-11-09 16:10:19 +0000

    responsive design

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -18,9 +18,9 @@ export const AboutPageTemplate = ({ title, content, contentComponent, portraitIm
               <PreviewCompatibleImage imageInfo={{ image: portraitImage }} style={{ borderRadius: '50%'}}/>
             </figure>
           </div>
-          <div className="column is-8">
-            <div className="section">
-              <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
+          <div  className="column is-8">
+            <div className="sectionAbout">
+              <h2  className="title is-size-3 has-text-weight-bold is-bold-light">
                 {title}
               </h2>
               <PageContent className="content" content={content} />
